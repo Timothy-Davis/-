02ada6230b386f2ede3df1deea4ea72a22c4afc0
Aaron Helton <aargonian@gmail.com>
2019-08-29 03:38:27 -0400

    Implemented a basic delegate system for the Main Menu
    To be honest, it's a lot more kludgy than I would like, but it works.

--- src/Main.py
+++ src/Main.py
@@ -13,36 +13,48 @@ UPDATES_PER_SEC = 60
 UPDATE_TIME = NS_PER_SEC/UPDATES_PER_SEC
 
 GAMES = [
-    #MainMenu()
 ]
+
+MAIN_MENU_STATE = 0
+GAME_STATE = 1
 
 def main():
     word_database.init()
     pygame.init()
     pygame.display.set_mode(DEFAULT_SIZE)
 
-    current_game: Game = MainMenu()
+    menu = MainMenu()
+    game = menu
 
+    done = False
     current_time = time.perf_counter_ns()
     lag = 0
-    done = False
+    state = MAIN_MENU_STATE
     while not done:
         old_time = current_time
         current_time = time.perf_counter_ns()
         lag += current_time - old_time
 
-        current_game.display()
-
+        game.display()
         for event in pygame.event.get():
-            current_game.handle_pygame_event(event)
+            game.handle_pygame_event(event)
 
         while lag > UPDATE_TIME:
-            current_game.update()
+            game.update()
             lag -= UPDATE_TIME
 
         pygame.display.flip()
 
-        done = current_game.is_done()
+        # If we are in the MAIN_MENU_STATE, we want to check and see if the
+        # main menu has a command to swap out to a game, or if the user wishes to quit entirely.
+        if state is MAIN_MENU_STATE:
+            done = menu.is_done()
+            if menu.selected_game() is not None:
+                game = menu.selected_game()
+                state = GAME_STATE
+        else:
+            if game.is_done():
+                state = MAIN_MENU_STATE
 
 if __name__ == '__main__':
     main()
--- src/main_menu.py
+++ src/main_menu.py
@@ -1,90 +1,161 @@
+from abc import abstractmethod, ABC
 import pygame
 import pygame.freetype
-from pygame.freetype import Font
 
-import game_base
 from game_base import Game
 
-TITLE_Y_PERCENTAGE = .20
-TITLE_FONT_SIZE = 48
-MENU_Y_PERCENTAGE = .50
-MENU_OPTION_OFFSET = 10
-MENU_FONT_SIZE = 24
+
+class _MenuOption:
+    def __init__(self, surface, x, y, callback, callback_argument):
+        self.x = x
+        self.y = y
+        self.surface = surface
+        self.callback = callback
+        self.callback_argument = callback_argument
+
+    def collision(self, x, y) -> bool:
+        if x < self.x:
+            return False
+        if y < self.y:
+            return False
+        if x > self.x + self.surface.get_width():
+            return False
+        if y > self.y + self.surface.get_height():
+            return False
+        return True
 
 
-class MainMenu(Game):
-    class _MenuOption:  # Nested Class because it isn't meant to be used anywhere else
-        def __init__(self, surface, x, y, callback):
-            self.x = x
-            self.y = y
-            self.surface = surface
-            self.callback = callback
+class MainMenuDelegate(ABC):
+    def __init__(self, parent, return_callback):
+        self.return_callback = return_callback
+        self.parent = parent
 
-        def collision(self, x, y) -> bool:
-            if x < self.x:
-                return False
-            if y < self.y:
-                return False
-            if x > self.x + self.surface.get_width():
-                return False
-            if y > self.y + self.surface.get_height():
-                return False
-            return True
+    @abstractmethod
+    def display(self) -> None:
+        pass
 
-    def __init__(self):
-        super().__init__(show_menu_bar=False)
-        self.font = pygame.freetype.Font("./fonts/NotoSansCJKjp-Regular.otf")
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle_event(self, event) -> None:
+        pass
+
+    def return_to_main_menu(self) -> None:
+        self.return_callback()
+
+
+class NormalMainMenu(MainMenuDelegate):
+    TITLE_Y_PERCENTAGE = .20
+    TITLE_FONT_SIZE = 48
+    MENU_Y_PERCENTAGE = .50
+    MENU_OPTION_OFFSET = 10
+    MENU_FONT_SIZE = 24
+
+    def __init__(self, parent, return_callback, menu_options):
+        super().__init__(parent, return_callback)
         self.title = '日本語のミニゲイム'
 
-        fgcolor = (255, 255, 255)
-        bgcolor = (0, 0, 0)
-        style = pygame.freetype.STYLE_DEFAULT
-        rotation = 0
-        size = TITLE_FONT_SIZE
-        def make_font_surface(text): return self.font.render(text, fgcolor, bgcolor, style, rotation, size)[0]
+        font_options = {
+            'fgcolor': (255, 255, 255),
+            'bgcolor': (0, 0, 0),
+            'style': pygame.freetype.STYLE_DEFAULT,
+            'rotation': 0,
+            'size': self.TITLE_FONT_SIZE,
+        }
+        def make_font_surface(text): return self.parent.font.render(text, **font_options)[0]
 
         self.title_surface = make_font_surface(self.title)
 
-        size = MENU_FONT_SIZE
-        self.title_x = self.surface.get_width() / 2 - self.title_surface.get_width() / 2
-        self.title_y = TITLE_Y_PERCENTAGE * self.surface.get_height()
+        font_options['size'] = self.MENU_FONT_SIZE
+        self.title_x = self.parent.surface.get_width() / 2 - self.title_surface.get_width() / 2
+        self.title_y = self.TITLE_Y_PERCENTAGE * self.parent.surface.get_height()
 
-        self.options = []
-        menu_options = [('Main Menu', self.handle_main_menu),
-                        ('Credits', self.handle_credits),
-                        ('Scores', self.handle_scores)]
         y_offset = 0
+        self.options = []
         for option in menu_options:
             surface = make_font_surface(option[0])
-            y = MENU_Y_PERCENTAGE * self.surface.get_height() + y_offset
-            x = self.surface.get_width()/2 - surface.get_width()/2
-            y_offset += surface.get_height() + MENU_OPTION_OFFSET
-            menu_option = self._MenuOption(surface, x, y, option[1])
+            y = self.MENU_Y_PERCENTAGE * self.parent.surface.get_height() + y_offset
+            x = self.parent.surface.get_width()/2 - surface.get_width()/2
+            y_offset += surface.get_height() + self.MENU_OPTION_OFFSET
+            menu_option = _MenuOption(surface, x, y, option[1], option[2])
             self.options.append(menu_option)
 
     def display(self):
-        self.surface.fill((0, 0, 0))
-        self.surface.blit(self.title_surface, (self.title_x, self.title_y))
+        self.parent.surface.fill((0, 0, 0))
+        self.parent.surface.blit(self.title_surface, (self.title_x, self.title_y))
         for option in self.options:
-            self.surface.blit(option.surface, (option.x, option.y))
+            self.parent.surface.blit(option.surface, (option.x, option.y))
 
     def handle_event(self, event) -> None:
         if event.type is pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             for option in self.options:
                 if option.collision(x, y):
-                    option.callback()
-
-        pass
+                    option.callback(option.callback_argument)
 
     def update(self):
         pass
 
-    def handle_main_menu(self):
-        print("Main Menu Clicked")
 
-    def handle_scores(self):
-        print("Scores Clicked")
+class MiniGameSelectMenu(MainMenuDelegate):
+    def __init__(self, parent, return_callback):
+        super().__init__(parent, return_callback)
+        self.red = 0
+        self.count = 3
 
-    def handle_credits(self):
-        print("Credits Clicked")
+    def display(self):
+        self.parent.surface.fill((self.red, 0, 0))
+
+    def handle_event(self, event) -> None:
+        pass
+
+    def update(self) -> None:
+        self.red += 1
+        self.red %= 256
+        if self.red == 0:
+            self.count -= 1
+        if self.count is 0:
+            self.return_to_main_menu()
+
+    def selected_game(self) -> Game:
+        return None
+
+class MainMenu(Game):
+    def __init__(self):
+        super().__init__(show_menu_bar=False)
+        self.font = pygame.freetype.Font("./fonts/NotoSansCJKjp-Regular.otf")
+
+        def return_callback(): self._change_state(None)
+        self.mini_game_select_delegate = MiniGameSelectMenu(self, return_callback)
+
+        menu_options = [
+            ("Mini Games", self._change_state, self.mini_game_select_delegate),
+            ("Credits", self._change_state, None),
+            ("Scores", self._change_state, None),
+        ]
+
+        self.main_menu = NormalMainMenu(self, None, menu_options)
+        self.delegate = self.main_menu
+
+    def display(self):
+        self.delegate.display()
+
+    def handle_event(self, event) -> None:
+        self.delegate.handle_event(event)
+
+    def update(self):
+        self.delegate.update()
+
+    def _change_state(self, delegate):
+        if delegate is not None:
+            self.delegate = delegate
+        else:
+            self.delegate = self.main_menu
+
+    def selected_game(self):
+        if self.delegate is not self.mini_game_select_delegate:
+            return None
+        else:
+            return self.delegate.selected_game()
